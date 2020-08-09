e1a0905e9c58bbe71bb1a655d6f002d894ae95cd
Svenia Behm <svenia.behm@uni-passau.de>
2020-08-09 14:01:52 +0200

    Modify data preparation; add column 'popDens' referring to population density approximated with gArea() based on data from BKG

--- Rcode/01-MonitoringSites_DE_DataPreparation.R
+++ Rcode/01-MonitoringSites_DE_DataPreparation.R
@@ -1,5 +1,5 @@
 ##########################################################################
-### Derive values of predictor variables for background sites in Germany
+### Derive values of predictor variables for monitoring sites in Germany
 ##########################################################################
 
 
@@ -42,12 +42,6 @@ which(names(df.meta)%in%names(df.NO2))
 df.NO2.meta <- merge(df.NO2,df.meta[,-c(1,3,4,5,8,11,12)],by="AirQualityStationEoICode")
 # 53 passive sampler extra -> number of observations rises from 1022 to 1075
 
-# Focus on background sites only
-#df.Background <- df.NO2.meta[df.NO2.meta$AirQualityStationType=="background",]
-
-unique(droplevels(df.NO2.meta$DatetimeBegin))
-unique(droplevels(df.NO2.meta$DatetimeEnd))
-
 # Focus on one year only
 df.NO2.meta <- df.NO2.meta[df.NO2.meta$DatetimeBegin=="2015-01-01 00:00:00",]
 
@@ -55,9 +49,9 @@ df.NO2.meta <- df.NO2.meta[df.NO2.meta$DatetimeBegin=="2015-01-01 00:00:00",]
 # Identify duplicates
 df.NO2.meta$AirQualityStationEoICode[duplicated(df.NO2.meta$AirQualityStationEoICode)]
 
-summary(df.NO2.meta$MeasurementType)
-#  active automatic   passive
-#       0       403       18
+table(df.NO2.meta$MeasurementType)
+# automatic   passive
+#       403       18
 #
 # Remove duplicates
 df.NO2.meta <- df.NO2.meta[df.NO2.meta$MeasurementType=="automatic",]
@@ -71,6 +65,9 @@ sPdf.boundaries.DE <- readOGR(dsn = "../DATA/Data_GADM", layer = "DEU_adm1")
 # EEA:CLC2012 layer ----
 first10.CLC12 <- readGDAL("../DATA/Data_CLC12/g100_clc12_V18_5a/g100_clc12_V18_5.tif",
                           region.dim = c(10,10))
+# CRS Name: EUR_ETRS89/LAEA1052 corresponds to EPSG:3035
+if(new_proj_and_gdal()) comment(slot(first10.CLC12, "proj4string"))
+
 
 sPdf.boundaries.DE <- spTransform(sPdf.boundaries.DE, CRS = proj4string(first10.CLC12))
 
@@ -107,9 +104,9 @@ sGdf.CLC12 <- readGDAL("../DATA/Data_CLC12/g100_clc12_V18_5a/g100_clc12_V18_5.ti
 
 
 
-# Write function to calculate shares of grouped land use classes within buffer of radius r
+# Calculate shares of grouped land use classes within buffer of radius r
 
-# Building buffers around monitoring sites with 1km, 5km and 21km and calculate shares of 10
+# Building buffers around monitoring sites with 1km and calculate shares of 10
 # grouped land use classes relying on Beelen et al.(2009)
 df.NO2.meta <- cbind(df.NO2.meta,data.frame(matrix(data = 0, nrow = nrow(df.NO2.meta), ncol = 10)))
 names(df.NO2.meta)[37:46] <- paste(rep("Class", n = 10), seq(from = 1, to = 10, by = 1), sep = "")
@@ -134,7 +131,7 @@ unique(df.NO2.meta$Projection)
 # +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs
 
 sp.sites <- SpatialPoints(coords = df.NO2.meta[,c("Longitude","Latitude")],
-                               proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+                          proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
 sp.sites <- spTransform(sp.sites,proj4string(raster.CLC12))
 
 radius <- 1000
@@ -168,9 +165,44 @@ admin.regions.2015 <- readOGR(dsn = "../DATA/Data_BKG/vg250-ew_ebenen",
                               encoding = "UTF-8",
                               use_iconv = TRUE)
 
+names(admin.regions.2015)
+# for our analysis the following variables are relevant
+# RS:
+# Regionalschlüssel (RS) gliedert sich wie folgt:
+# -> 1.-2. digit = identification number of the federal state
+# -> 3.digit = identification number of the administrative district (Regierungsbezirk)
+# -> 4.–5.digit = identification number of the district or county (Kreis)
+# -> 6.–9.digit = identification number of the administrative association (Verwaltungsgemeinschaft)
+# -> 10.–12.digit = community identification number (Gemeinde)
+# Note: In case the 6th digit is '9' it refers to a municipality not adhering to an administrative association
+# Then the municipality is associated with the degree of urbanisation 'rural'
+
+# AGS: official municipality key (Amtlicher Gemeindeschlüssel)
+# -> 1.-2. digit: identification number of the federal state
+# -> 3. digit: identification number of the administrative district (Regierungsbezirk),
+# -> 4.-5. digit: identification number of the district or county (Kreis)
+# -> 6.-8. digit: community identification number (Gemeinde)
+
+# GEN: Geografischer Name
+# NUTS: Europäischer Statistikschlüssel
+# EWZ: number of inhabitants (Einwohnerzahl)
+admin.regions.2015 <- admin.regions.2015[,c("RS", "AGS", "GEN", "NUTS", "EWZ")]
+
+# Calculate area of each polygon in 'admin.regions.2015' and add column to 'admin.regions.2015@data'
+proj4string(admin.regions.2015) # unit=m
+# gArea() returns area in m^2, divide by 1000*1000 to get area in km^2
+admin.regions.2015$Area_km2 <- gArea(admin.regions.2015, byid = TRUE) / 1000000
+
+admin.regions.2015$popDens <- as.numeric(admin.regions.2015$EWZ) / admin.regions.2015$Area_km2
+summary(admin.regions.2015$popDens)
+
+
+# Note that calculating area of municipalities based on the respective polygon is just an approximation as
+# a polygon is built upon connecting points and resembles the shape of the respective municipality not exactly.
+# More exact values for the population density can be retrieved using data from BBSR...
+
 
 # BBSR (Bundesinstitut für Bau-, Stadt- und Raumforschung) ----
-# Urban-rural typology of the German administrative regions
 # data 'Gem15-Raumtyp.xlsx' received via E-Mail from Petra Kuhlmann on 19th July 2017
 # Raumtypen beziehen sich auf die Raumordnung von 2010
 # Die Zuordnung zu den Gemeinden bezieht sich auf den Gemeindestand 2015-12-31
@@ -195,109 +227,76 @@ df.popDens2015 <- read.xlsx("../DATA/Data_BBSR/Gem15-Raumtyp.xlsx",
 names(df.area2015)
 names(df.popDens2015)
 
-names(admin.regions.2015)
-# an overview over the attributes is given in the Pdf "Produktdokumentation VG250"
-# for our analysis the following variables are relevant
-# RS:
-# Regionalschlüssel (RS) gliedert sich wie folgt:
-# -> 1.-2. digit = identification number of the federal state
-# -> 3.digit = identification number of the administrative district (Regierungsbezirk)
-# -> 4.–5.digit = identification number of the district or county (Kreis)
-# -> 6.–9.digit = identification number of the administrative association (Verwaltungsgemeinschaft)
-# -> 10.–12.digit = community identification number (Gemeinde)
-# Note: In case the 6th digit is '9' it refers to a municipality not adhering to an administrative association
-# Then the municipality is associated with the degree of urbanisation 'rural'
-
-# AGS: official municipality key (Amtlicher Gemeindeschlüssel)
-# -> 1.-2. digit: identification number of the federal state
-# -> 3. digit: identification number of the administrative district (Regierungsbezirk),
-# -> 4.-5. digit: identification number of the district or county (Kreis)
-# -> 6.-8. digit: community identification number (Gemeinde)
-
-# GEN: Geografischer Name
-# NUTS: Europäischer Statistikschlüssel
-
-admin.regions.2015.sub <- admin.regions.2015[,names(admin.regions.2015) %in% c("RS", "AGS", "GEN", "NUTS")]
 
 dummy.tmp <- nchar(df.area2015$gem15) == 7
 df.area2015$gem15_full[dummy.tmp] <- paste(0,df.area2015$gem15[dummy.tmp],sep="")
 df.area2015$gem15_full[!dummy.tmp] <- df.area2015$gem15[!dummy.tmp]
-all(admin.regions.2015.sub$AGS %in% df.area2015$gem15_full)
-test2 <- admin.regions.2015.sub$AGS %in% df.area2015$gem15_full
-table(test2)
+all(admin.regions.2015$AGS %in% df.area2015$gem15_full)
+table(admin.regions.2015$AGS %in% df.area2015$gem15_full)
 # there are 185 municipalities for which no entry is available in `df.area`
-x1 <- admin.regions.2015.sub[!test2,]@data
-#View(x1)
+# View(admin.regions.2015[!(admin.regions.2015$AGS %in% df.area2015$gem15_full),]@data)
 
 
 # Data\Data_BKG\dokumentation\vg250.pdf
 # search for 'gemeindefrei' -> Regionalschlüssel (RS) 6.Stelle
-x1$check9 <- sapply(as.vector(x1[,1]), function(x) substring(x, 6,6))
-
-# sort for 'check9'
-# add to all municipalities except 'Berlin' and 'Hamburg' attribute 'rural'
-admin.regions.2015.sub$digit6 <- sapply(as.vector(admin.regions.2015.sub$RS), function(x) substring(x,6,6))
+admin.regions.2015$digit6 <- sapply(as.vector(admin.regions.2015$RS), function(x) substring(x,6,6))
 
 
-regions.2015.complete <- merge(admin.regions.2015.sub,df.area2015,
-                               by.y = "gem15_full", by.x = "AGS", all.x = TRUE)
+regions.2015 <- merge(admin.regions.2015, df.area2015[,c("vbgem15", "gem15_full")],
+                      by.y = "gem15_full", by.x = "AGS", all.x = TRUE)
 
 
+View(regions.2015@data[is.na(regions.2015$vbgem15), ])
 
-regions.2015.complete$raumt2010besiedlung[regions.2015.complete$digit6==9] <- "überwiegend ländlich"
+# Add manually the information for 'Berlin' and 'Hamburg' contained in in df.area.2015
+df.area2015[df.area2015$name15%in%c("Berlin, Stadt", "Hamburg, Freie und Hansestadt"),]
+View(regions.2015@data[is.na(regions.2015$vbgem15),]) # filter for  digit == 0
+regions.2015@data[is.na(regions.2015$vbgem15) & regions.2015$digit6==0, "vbgem15"] <- df.area2015[c(1113,1113,8497),"vbgem15"]
 
-# Add manually the information for 'Berlin' and 'Hamburg' contained in in dfarea.2015
-df.area2015[df.area2015$name15%in%c("Berlin, Stadt", "Hamburg, Freie und Hansestadt"),1:7]
-View(regions.2015.complete@data[is.na(regions.2015.complete$raumt2010besiedlung),])
-
-regions.2015.complete@data[is.na(regions.2015.complete$raumt2010besiedlung),6:12] <- df.area2015[c(1113,1113,8497),1:7]
-
-# Are there any more NA's?
-check.na <- is.na(regions.2015.complete$raumt2010besiedlung)
-table(check.na)
-# No! -> to every polygon in 'admin.regions.2015.sub' the corresponding degree of urbanisation can be attributed
-
-levels(regions.2015.complete$raumt2010besiedlung)
-levels(regions.2015.complete$raumt2010besiedlung) <- c("suburban", "rural", "urban")
+table(is.na(regions.2015$vbgem15))
+# now there are 182 entries in 'regions.2015' without a value for 'vbgem15'
+# these entries refer to regions as forests where in general no people live
 
 
-proj4string(regions.2015.complete)
+proj4string(regions.2015)
 proj4string(sp.sites)
 
-
-df.NO2.meta2 <- cbind(df.NO2.meta,over(spTransform(sp.sites,proj4string(regions.2015.complete)),regions.2015.complete))
-summary(df.NO2.meta2$AGS)
-df.NO2.meta2[is.na(df.NO2.meta2$AGS),]
+df.NO2.meta2 <- cbind(df.NO2.meta,over(spTransform(sp.sites,CRS(proj4string(regions.2015))),regions.2015))
+summary(df.NO2.meta2$popDens)
+df.NO2.meta2[is.na(df.NO2.meta2$popDens),]
 # Search on the internet for monitoring sites "DESN024" and "DEMV031" -> they are located in Klingenthal and Rostock, respectively
 # Search on the internet for the AGS of Klingenthal and Rostock -> attributed to "14523160" and "13003000", respectively
-# Why are they not attributed to a polygon in "regions.2015.complete"?
-plot(regions.2015.complete[regions.2015.complete$AGS=="14523160",])
-plot(regions.2015.complete[regions.2015.complete$AGS=="13003000",])
+# Why are they not attributed to a polygon in "regions.2015"?
+plot(regions.2015[regions.2015$AGS=="14523160",])
+plot(regions.2015[regions.2015$AGS=="13003000",])
 which(is.na(df.NO2.meta2$AGS))
 # [1] 200 340
-plot(spTransform(sp.sites[200,],proj4string(regions.2015.complete)),col="red",pch=16,cex=1,add=TRUE)
+plot(spTransform(sp.sites[200,],proj4string(regions.2015)),col="red",pch=16,cex=1,add=TRUE)
 # Because it is on the border of the respective polygon...
 # Add the information manually
 names(df.NO2.meta2)
-names(regions.2015.complete)
-df.NO2.meta2[df.NO2.meta2$AirQualityStationEoICode=="DESN024",47:58] <- regions.2015.complete@data[regions.2015.complete$AGS=="14523160",]
-df.NO2.meta2[df.NO2.meta2$AirQualityStationEoICode=="DEMV031",47:58] <- regions.2015.complete@data[regions.2015.complete$AGS=="13003000",][-1,]
-
+names(regions.2015)
+df.NO2.meta2[df.NO2.meta2$AirQualityStationEoICode=="DESN024",47:55] <- regions.2015@data[regions.2015$AGS=="14523160",]
+df.NO2.meta2[df.NO2.meta2$AirQualityStationEoICode=="DEMV031",47:55] <- regions.2015@data[regions.2015$AGS=="13003000",][-1,]
 
 
 summary(duplicated(df.popDens2015$vbgem15)) # unambiguously identified
 df.NO2.meta3 <- merge(df.NO2.meta2, df.popDens2015[,c(1,3,4)], by="vbgem15",all.x=TRUE)
-df.NO2.meta3$popDens15 <- df.NO2.meta3$z_bev15/df.NO2.meta3$fl15_sum
+df.NO2.meta3$BBSRpopDens <- df.NO2.meta3$z_bev15/df.NO2.meta3$fl15_sum
 
+summary(df.NO2.meta3$BBSRpopDens)
 
+# Compare popDens derived with data from BKG only (gArea()) with BBSRpopDens
+plot(popDens ~ BBSRpopDens, data = df.NO2.meta3)
+abline(c(0,1), col = "blue", lwd = 2)
 
 
 
 # Create indicator vector for filtering AGS referring to Rhine-Ruhr region
-names(regions.2015.complete)
-unique(regions.2015.complete$AGS)
+names(regions.2015)
+unique(regions.2015$AGS)
 
-regions.2015.NRW <- subset(regions.2015.complete, substr(regions.2015.complete$AGS, 1,2 ) == "05")
+regions.2015.NRW <- subset(regions.2015, substr(regions.2015$AGS, 1,2 ) == "05")
 k.ME <- as.character(regions.2015.NRW$AGS[substr(regions.2015.NRW$AGS, 3, 5) == "158"])
 k.UN <- as.character(regions.2015.NRW$AGS[substr(regions.2015.NRW$AGS, 3, 5) == "978"])
 k.EN <- as.character(regions.2015.NRW$AGS[substr(regions.2015.NRW$AGS, 3, 5) == "954"])[-4]
@@ -313,7 +312,7 @@ k.WES <- as.character(regions.2015.NRW$AGS[substr(regions.2015.NRW$AGS, 3, 5) ==
 cities <- as.character(regions.2015.NRW$AGS[substr(regions.2015.NRW$AGS, 6, 8) %in% c("000")])[-c(3,16)]
 
 ind.RR <- c(k.ME, k.UN, k.EN, k.MK, k.RE, k.BM, k.NE, k.SU, k.GL, k.VIE, k.WES, cities)
-#saveRDS(object = ind.RR, file = "smoothLUR/indRhineRuhr.rds")
+#saveRDS(object = ind.RR, file = "indRhineRuhr.rds")
 
 
 
@@ -426,79 +425,22 @@ plot(sLdf.tmp2.14,col="red",add=TRUE,lty=2,lwd=3)
 
 
 
-LinesLength(sLdf.tmp2@lines[[1]]) + LinesLength(sLdf.tmp2@lines[[2]])
 sum(SpatialLinesLengths(sLdf.tmp2.14))
 sum(SpatialLinesLengths(sLdf.tmp2))
 sum(SpatialLinesLengths(spTransform(sLdf.tmp,proj4string(sLdf.tmp2))))
 
 
 
-## Derive distance of each monitroing site to next road
-sLdf.roads.DE2 <- spTransform(sLdf.roads.DE, proj4string(sp.sites3))
-
-sLdf.roads.DE2.14 <- subset(sLdf.roads.DE2, sLdf.roads.DE2$RTT == 14)
-sLdf.roads.DE2.15 <- subset(sLdf.roads.DE2, sLdf.roads.DE2$RTT == 15)
-sLdf.roads.DE2.16 <- subset(sLdf.roads.DE2, sLdf.roads.DE2$RTT == 16)
-sLdf.roads.DE2.984 <- subset(sLdf.roads.DE2, sLdf.roads.DE2$RTT == 984)
-
-dat.ProximityToRoads <- data.frame(ID = df.NO2.meta3$AirQualityStationEoICode,
-                                   Type = df.NO2.meta3$AirQualityStationType,
-                                   Lon.WGS84 = df.NO2.meta3$Longitude,
-                                   Lat.WGS84 = df.NO2.meta3$Latitude,
-                                   DistPri = rep(NA, nrow(df.NO2.meta3)),
-                                   DistSec = rep(NA, nrow(df.NO2.meta3)),
-                                   DistMot = rep(NA, nrow(df.NO2.meta3)),
-                                   DistLoc = rep(NA, nrow(df.NO2.meta3)))
-
-
-
-for(i in 1:nrow(df.NO2.meta3)){
-  dat.ProximityToRoads$DistPri[i] <- gDistance(sp.sites3[i,], sLdf.roads.DE2.14)
-  dat.ProximityToRoads$DistSec[i] <- gDistance(sp.sites3[i,], sLdf.roads.DE2.15)
-  dat.ProximityToRoads$DistMot[i] <- gDistance(sp.sites3[i,], sLdf.roads.DE2.16)
-  dat.ProximityToRoads$DistLoc[i] <- gDistance(sp.sites3[i,], sLdf.roads.DE2.984)
-}
-
-
-dat.ProximityToRoads[,5:8] <- round(dat.ProximityToRoads[,5:8], 2)
-
-write.csv(dat.ProximityToRoads, file = "R/DATA/Data_built/dat_ProximityToRoads_background.csv")
-
-
-# Choose i and check if for-loop does what it is supposed to do...
-i <- 33
-Buffer.tmp <- gBuffer(sp.sites3[i, ], width = 7000)
-sLdf.tmp2 <- crop(sLdf.roads.DE2, Buffer.tmp)
-sLdf.tmp2.14 <- subset(sLdf.tmp2, sLdf.tmp2$RTT==14)
-sLdf.tmp2.15 <- subset(sLdf.tmp2, sLdf.tmp2$RTT==15)
-sLdf.tmp2.16 <- subset(sLdf.tmp2, sLdf.tmp2$RTT==16)
-sLdf.tmp2.984 <- subset(sLdf.tmp2, sLdf.tmp2$RTT==984)
-
-
-par(mar=c(4,4,4,4))
-plot(Buffer.tmp)
-plot(sp.sites3[i,],add=TRUE)
-plot(sLdf.tmp2.14,col="blue",add=TRUE,lwd=2)
-plot(sLdf.tmp2.15,col="red",add=TRUE,lty=2,lwd=3)
-plot(sLdf.tmp2.16,col="green",add=TRUE,lty=2,lwd=4)
-plot(sLdf.tmp2.984,col="orange",add=TRUE,lty=2,lwd=5)
-# -> Compare with GoogleMaps
-
-
-
-
-
 # Select columns required for analysis ----
 names(df.NO2.meta3)
-df.analysis <- df.NO2.meta3[,c(2,11,24,26:29,31:32,35,38:48,50:51,57:58,61:65)]
+df.analysis <- df.NO2.meta3[,c(2,11,24,26:29,31:32,35,38:48,50:51,54,58:62)]
 df.analysis$ObservationDateBegin = 2015
 
 # EoI: Exchange on Information
-names(df.analysis)[c(1:3,5:20,24:30)] <- c("AQeCode", "AQeYMean", "Year", "AQeLon", "AQeLat", "AQeAlt",
+names(df.analysis)[c(1:3,5:20,26:29)] <- c("AQeCode", "AQeYMean", "Year", "AQeLon", "AQeLat", "AQeAlt",
                                            "AQeType", "AQeArea", "AQeInletHeight",
                                            "HighDens", "LowDens", "Ind", "Transp", "Seap",
                                            "Airp", "Constr", "UrbGreen", "Agri", "Forest",
-                                           "BBSRArea","BBSRArea2","BBSRpopDens",
                                            "PriRoad", "SecRoad", "NatMot", "LocRoute")
 
-write.csv(df.analysis, file = "R/DATA/Data_built/DATA_MonitoringSites_DE.csv")
+write.csv(df.analysis, file = "DATA_MonitoringSites_DE.csv")
