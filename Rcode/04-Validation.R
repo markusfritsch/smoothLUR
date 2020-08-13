71e3dec3279cdba0d4c5616f8cabb5fed43c9137
Svenia Behm <svenia.behm@uni-passau.de>
2020-08-13 10:38:00 +0200

    Complement validation of models for background monitoring sites

--- Rcode/04-Validation.R
+++ Rcode/04-Validation.R
@@ -4,7 +4,7 @@
 
 
 # Validation techniques:
-# loocv, 10-fold cv, and hold-out validation
+# loocv, (stratified) 10-fold cv, and (stratified) hold-out validation
 
 rm(list = ls())
 
@@ -39,9 +39,19 @@ loocv.b <- kFoldCV(data = dat[dat$AQeType=="background",]
                    ,seed = 1
                    ,k = 10
                    ,strat = FALSE
+                   ,indRegions = "indRegions"
                    ,loocv = TRUE
                    )
-Sys.time()
+rmse_looCV_par_b <- sqrt(mean(loocv.b$df.err$Err.par^2))
+aic_looCV_par_b  <- mean(sapply(loocv.b$ls.models, FUN = function(f) AIC(f$mod.par)))
+adjR2_looCV_par_b  <-  mean(sapply(loocv.b$ls.models, FUN = function(f) summary(f$mod.par)$adj.r.squared))
+
+rmse_looCV_smooth_b <- sqrt(mean(loocv.b$df.err$Err.smooth^2))
+aic_looCV_smooth_b <- mean(sapply(loocv.b$ls.models, FUN = function(f) AIC(f$mod.smooth)))
+adjR2_looCV_smooth_b  <-  mean(sapply(loocv.b$ls.models, FUN = function(f) summary(f$mod.smooth)$r.sq))
+
+
+
 
 
 
@@ -69,6 +79,7 @@ for(l in 1:100){
                                   ,seed = l
                                   ,k = 10
                                   ,strat = FALSE
+                                  ,indRegions = "indRegions"
                                   ,loocv = FALSE
                                   )
 }
@@ -109,4 +120,93 @@ mean(adjR2_TenFoldCV_par_b); mean(adjR2_TenFoldCV_smooth_b)
 plot(density(rmse_TenFoldCV_smooth_b), xlim = c(min(rmse_TenFoldCV_smooth_b),max(rmse_TenFoldCV_par_b)),
      col = "orange", lwd = 2)
 lines(density(rmse_TenFoldCV_par_b), col = "blue", lwd = 2)
+
+
+
+
+###
+## Hold-out validation
+###
+
+# can easily be derived based on the results of 10-fold CV by using only Fold1 as test sample
+
+rmse_HoldOutCV_par_b <- sapply(listTenFoldCV.b, FUN = function(x) sqrt(mean(x$df.err$Err.par[x$df.err$Fold==1]^2)))
+aic_HoldOutCV_par_b  <- sapply(listTenFoldCV.b, FUN = function(s) AIC(s$ls.models$Fold1$mod.par))
+adjR2_HoldOutCV_par_b  <- sapply(listTenFoldCV.b, FUN = function(s) summary(s$ls.models$Fold1$mod.par)$adj.r.squared)
+
+rmse_HoldOutCV_smooth_b <- sapply(listTenFoldCV.b, FUN = function(x) sqrt(mean(x$df.err$Err.smooth[x$df.err$Fold==1]^2)))
+aic_HoldOutCV_smooth_b  <- sapply(listTenFoldCV.b, FUN = function(s) AIC(s$ls.models$Fold1$mod.smooth))
+adjR2_HoldOutCV_smooth_b  <- sapply(listTenFoldCV.b, FUN = function(s) summary(s$ls.models$Fold1$mod.smooth)$r.sq)
+
+mean(rmse_HoldOutCV_par_b); mean(rmse_HoldOutCV_smooth_b)
+mean(aic_HoldOutCV_par_b); mean(aic_HoldOutCV_smooth_b)
+mean(adjR2_HoldOutCV_par_b); mean(adjR2_HoldOutCV_smooth_b)
+
+
+
+
+
+###
+## Stratified 10-fold cross-validation
+###
+
+# Apply function kFoldCV for seed values 1 to 100
+listStratTenFoldCV.b <- vector("list", length = 100)
+names(listStratTenFoldCV.b) <- paste("seed",1:100,sep="")
+
+for(l in 1:100){
+  listStratTenFoldCV.b[[l]] <- kFoldCV(data = dat[dat$AQeType=="background",]
+                                  ,pred = c("AQeLon", "AQeLat", "AQeAlt", "HighDens"
+                                            ,"LowDens", "Ind", "Transp", "Seap", "Airp"
+                                            ,"Constr", "UrbGreen", "Agri", "Forest"
+                                            , "popDens", "PriRoad", "SecRoad", "NatMot"
+                                            , "LocRoute")
+                                  ,ID = "AQeCode"
+                                  ,spVar1 = "AQeLon"
+                                  ,spVar2 = "AQeLat"
+                                  ,depVar = "AQeYMean"
+                                  ,dirEff = c(0,0,-1,1,1,1,1,1,1,1,-1,0,-1,1,1,1,1,1)
+                                  ,thresh = 0.95
+                                  ,seed = l
+                                  ,k = 10
+                                  ,strat = TRUE
+                                  ,indRegions = "indRegions"
+                                  ,loocv = FALSE
+  )
+}
+
+rmse_StratTenFoldCV_par_b <- sapply(listStratTenFoldCV.b, FUN = function(x) sqrt(mean(x$df.err$Err.par^2)))
+aic_StratTenFoldCV_par_b  <- sapply(listStratTenFoldCV.b, FUN = function(s) {
+  AIC_seed_tmp <- sapply(s$ls.models, FUN = function(f) {
+    return(AIC(f$mod.par))
+  })
+  return(mean(AIC_seed_tmp))
+})
+adjR2_StratTenFoldCV_par_b  <- sapply(listStratTenFoldCV.b, FUN = function(s) {
+  adjR2_seed_tmp <- sapply(s$ls.models, FUN = function(f) {
+    return(summary(f$mod.par)$adj.r.squared)
+  })
+  return(mean(adjR2_seed_tmp))
+})
+
+rmse_StratTenFoldCV_smooth_b <- sapply(listStratTenFoldCV.b, FUN = function(x) sqrt(mean(x$df.err$Err.smooth^2)))
+aic_StratTenFoldCV_smooth_b  <- sapply(listStratTenFoldCV.b, FUN = function(s) {
+  AIC_seed_tmp <- sapply(s$ls.models, FUN = function(f) {
+    return(AIC(f$mod.smooth))
+  })
+  return(mean(AIC_seed_tmp))
+})
+adjR2_StratTenFoldCV_smooth_b  <- sapply(listStratTenFoldCV.b, FUN = function(s) {
+  adjR2_seed_tmp <- sapply(s$ls.models, FUN = function(f) {
+    return(summary(f$mod.smooth)$r.sq)
+  })
+  return(mean(adjR2_seed_tmp))
+})
+
+mean(rmse_StratTenFoldCV_par_b); mean(rmse_StratTenFoldCV_smooth_b)
+mean(aic_StratTenFoldCV_par_b); mean(aic_StratTenFoldCV_smooth_b)
+mean(adjR2_StratTenFoldCV_par_b); mean(adjR2_StratTenFoldCV_smooth_b)
+
+
+
 
