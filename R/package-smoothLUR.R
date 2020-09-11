31cf5b5acd0ecd22bf72795bed5242f151fb9627
markusfritsch <59094275+markusfritsch@users.noreply.github.com>
2020-09-11 10:31:43 +0200

    adjust package-smoothLUR.R

--- R/package-smoothLUR.R
+++ R/package-smoothLUR.R
@@ -1,9 +1,9 @@
 #' smoothLUR: A package for smooth land use regression modeling
 #'
-#' The smoothLUR package provides function for smooth land use
+#' The smoothLUR package provides functions for smooth land use
 #' regression modeling and parametric benchmarks. The main
 #' functions of the package are \code{smoothLUR}, \code{escapeLUR},
-#' and the evaluation function \code{kFoldcvLUR}.
+#' and the evaluation functions \code{kFoldCV} and \code{looCV}.
 #'
 #' @docType package
 #' @name smoothLUR
